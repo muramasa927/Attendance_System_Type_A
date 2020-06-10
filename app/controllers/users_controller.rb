3715a6ce8409af62dcfaf3011ba81f1787da739c
muramasa <snight16@icloud.com>
2020-06-10 11:00:52 +0000

    Add before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def show
-    @user = User.find(params[:id])  
   end
   
   def new
@@ -19,10 +22,43 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました"
+      redirect_to @user
+    else
+      render :edit
+    end
+    
+  end
+  
   # プライベート
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # beforeフィルタ
+    
+    # ユーザーを取得する
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    # ログイン済みのユーザーか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+    
+    # 現在ログインしているユーザーか確認
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,7 +30,18 @@ module SessionsHelper
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
+  end
+  
+  # ログイン済みのユーザーであればtrue
+  def current_user?(user)
+    user == current_user
   end
 
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
