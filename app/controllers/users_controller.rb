c5238b2b690b2a5571694484aa11029c8476118e
muramasa <snight16@icloud.com>
2020-06-12 12:38:28 +0000

    Add Paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   
   def show
   end
@@ -52,6 +56,7 @@ class UsersController < ApplicationController
     # ログイン済みのユーザーか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "ログインしてください"
         redirect_to login_url
       end
