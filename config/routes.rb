c45cb7ebab0dab6421266099db4e656b26f9786a
中村仁宣 <nakamuramasanobu@nakamurajinsennoMacBook-Pro.local>
2020-09-14 20:58:56 +0900

    20200914-touch-edit-overtime-confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,12 @@ Rails.application.routes.draw do
       patch 'attendances/update_one_month'
     end
     resources :attendances, only: :update 
-    get 'attendances/:id/edit_overtime_application', to: 'attendances#edit_overtime_application', as: :attendances_edit_overtime_application
-    patch 'attendances/:id/update_overtime_application', to: 'attendances#update_overtime_application', as: :attendances_update_overtime_application
+    get 'attendances/:id/edit_overtime_application', 
+      to: 'attendances#edit_overtime_application', as: :attendances_edit_overtime_application
+    patch 'attendances/:id/update_overtime_application', 
+      to: 'attendances#update_overtime_application', as: :attendances_update_overtime_application
+    get 'attendances/edit_overtime_confirmation'
+    patch 'attendances/update_overtime_confirmation'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 20200908104828) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-08-29 23:00:00"
-    t.datetime "work_time", default: "2020-08-29 22:30:00"
+    t.datetime "basic_time", default: "2020-09-12 23:00:00"
+    t.datetime "work_time", default: "2020-09-12 22:30:00"
     t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
